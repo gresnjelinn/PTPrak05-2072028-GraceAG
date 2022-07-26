05e1a7b444edfbb10d6b709137c96c1d03e7ced9
GraceAngelina626 <71739797+GraceAngelina626@users.noreply.github.com>
2022-07-26 15:09:25 +0700

    Menambahkan accelerator dan commit rollback

---
+++ pt05-prak-2072028-jdbc/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.pt04prak2072028jdbc {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+    opens com.example.pt04prak2072028jdbc to javafx.fxml;
+    exports com.example.pt04prak2072028jdbc;
+    exports com.example.pt04prak2072028jdbc.util;
+    exports com.example.pt04prak2072028jdbc.model;
+    exports com.example.pt04prak2072028jdbc.dao;
+}
